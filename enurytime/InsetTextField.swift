a95640a8e0831c6aa01f7e0f9cd0444d57e6df35
yerin <dpfls1022@naver.com>
2024-07-28 23:55:51 +0900

    아이디, 비밀번호 입력란 생성

--- enurytime/InsetTextField.swift
+++ enurytime/InsetTextField.swift
@@ -1,30 +1,13 @@
 //
-//  InsetTextField.swift
+//  InsertTextField.swift
 //  enurytime
 //
-//  Created by 박예린 on 7/26/24.
+//  Created by 박예린 on 7/28/24.
 //
 
+import Foundation
 import UIKit
 
 class InsetTextField: UITextField {
-    var insetX: CGFloat = 6 {
-        didSet {
-            layoutIfNeeded()
-        }
-    }
-    
-    var insetY: CGFloat = 6 {
-        didSet {
-            layoutIfNeeded()
-        }
-    }
-    
-    override func textRect(forBounds bounds: CGRect) -> CGRect {
-        return bounds.insetBy(dx: insetX , dy: insetY)
-    }
-    
-    override func editingRect(forBounds bounds: CGRect) -> CGRect {
-        return bounds.insetBy(dx: insetX , dy: insetY)
-    }
+    var insert
 }
