c6f17ad06ab6f39133667ec7d7fe47f5bb5aae55
yerin <dpfls1022@naver.com>
2024-08-08 23:47:47 +0900

    MainView 완성 (TableView, Cell, NavitionBar 익히기)

---
+++ enurytime/MainViewController.swift
@@ -0,0 +1,8 @@
+//
+//  MainViewController.swift
+//  enurytime
+//
+//  Created by 박예린 on 8/2/24.
+//
+
+import Foundation
---
+++ enurytime/NavigationTitleView.swift
@@ -0,0 +1,8 @@
+//
+//  NavigationTitleView.swift
+//  enurytime
+//
+//  Created by 박예린 on 8/8/24.
+//
+
+import Foundation
